b5ddf890f949773eed10c71805451a38c592be6c
Srđan Napijalo <52132674+srdjan-napijalo@users.noreply.github.com>
2022-03-02 13:41:18 +0100

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Block(){
+    return <div className='box--field'></div>
+}
+
+export default Block;
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import Game from './Game';
+
+
+
+
+
+ReactDOM.render(<Game />,document.getElementById('root'));
+
+
